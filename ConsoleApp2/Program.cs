6904dc8d3f1478ea20dba9d610b6d1d7de514323
Maksonch1k <123887520+Maksonch1k@users.noreply.github.com>
2024-05-20 09:43:51 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Point<int> intPoint = new Point<int>(3, 4);
+            //Point<uint> uintPoint = new Point<uint>(5u, 6u);
+            //Point<double> doublePoint = new Point<double>(6, 7);
+
+            //intPoint.Print();
+            //uintPoint.Print();
+            //doublePoint.Print();
+
+            //intPoint.X = 10;
+            //intPoint.Y = 20;
+            //intPoint.Print();
+
+            //Console.WriteLine("введите сторону а");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("введите сторону b");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("введите сторону c");
+            //int c = Convert.ToInt32(Console.ReadLine());
+
+
+            
+            Console.ReadKey();
+        }
+    }
+}
